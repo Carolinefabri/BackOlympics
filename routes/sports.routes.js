0a9053e9e0fb572a1d1bfec129170a2cb8a16e4a
“Caroline <carolinefabri.dev@hotmail.com>
2023-08-04 17:41:25 +0200

    routes sports probably done

--- routes/sports.routes.js
+++ routes/sports.routes.js
@@ -1,32 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const Sport = require('../models/Sport.model');
-const { Client } = require('@googlemaps/google-maps-services-js');
 const SportData = require ('../api/dbData.json');
 
-// Create a client for the Google Maps API with your API key
-const googleMapsClient = new Client({ apiKey: process.env.GOOGLE_MAPS_API_KEY });
-
-
-// Helper function to fetch restaurant suggestions
-async function getRestaurantSuggestions(location) {
-  const response = await googleMapsClient.placesNearby({
-    params: {
-      location: location,
-      radius: 1000, // You can adjust the radius as needed
-      type: 'restaurant', // You can specify other types of places here
-      key: process.env.GOOGLE_MAPS_API_KEY,
-    },
-  });
-
-  // Return the list of restaurant suggestions
-  return response.data.results;
-}
 
 
 
 // Route to get all sports with restaurant suggestions
-router.get('/', async (req, res) => {
+router.get('/sports', async (req, res) => {
   try {
 console.log("esta ceeeeerto");
     res.json(SportData);
@@ -41,17 +22,19 @@ console.log("esta ceeeeerto");
 router.get('/:id', async (req, res) => {
  
     const sportId = req.params.id;
-
-    
   const filterSports = SportData.sports.filter(sport => sport.id == req.params.id
       )
         res.json(filterSports);
 
   
 });
-
-
-
+// Route to get an individual sport by name 
+router.get('/name/:name', async (req, res) => {
+  const sportName = req.params.name;
+  const filterSports = SportData.sports.filter(sport => sport.name == sportName);
+  console.log('Filtering sports by name:', sportName);
+  res.json(filterSports);
+});
 
 
 module.exports = router;
