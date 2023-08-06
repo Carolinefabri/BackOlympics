93565ed2f19c00f212c29d75dc4bf7aa59bdd4c2
Brian Adams <brianadamsmaino68@gmail.com>
2023-08-06 19:46:23 +0200

    Nothing

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { v4: uuidv4 } = require('uuid');
-const multer = require('multer'); // Importar o multer
-const path = require('path'); // Importar o módulo path
-const fs = require('fs'); // Importar o módulo fs
-const User = require('../models/User.model');
-const { isAuthenticated } = require('../middlewares/jwt.middleware');
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { v4: uuidv4 } = require("uuid");
+const multer = require("multer"); // Importar o multer
+const path = require("path"); // Importar o módulo path
+const fs = require("fs"); // Importar o módulo fs
+const User = require("../models/User.model");
+const { isAuthenticated } = require("../middlewares/jwt.middleware");
 
 const router = express.Router();
 
 // Criar o diretório 'uploads' se não existir
-const uploadDir = './uploads';
+const uploadDir = "./uploads";
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir);
 }
@@ -19,11 +19,11 @@ if (!fs.existsSync(uploadDir)) {
 // Configurar o multer para fazer o upload de arquivos
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './uploads/'); // O caminho onde os arquivos serão salvos
+    cb(null, "./uploads/"); // O caminho onde os arquivos serão salvos
   },
   filename: function (req, file, cb) {
     // Definir o nome do arquivo como um UUID + a extensão original do arquivo
-    const uniqueSuffix = Date.now() + '-' + uuidv4();
+    const uniqueSuffix = Date.now() + "-" + uuidv4();
     cb(null, uniqueSuffix + path.extname(file.originalname));
   },
 });
@@ -31,30 +31,19 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 // Defina as rotas aqui
-router.get('/all', isAuthenticated, async (req, res) => {
+router.get("/all", isAuthenticated, async (req, res) => {
   try {
     const users = await User.find();
     res.status(200).json(users);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: 'Error fetching users' });
+    res.status(500).json({ error: "Error fetching users" });
   }
 });
 // Exporte o router
 module.exports = router;
 
-// GET route to get all users
-router.get('/all', async (req, res) => {
-  try {
-    const users = await User.find();
-    res.status(200).json(users);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: 'Error fetching users' });
-  }
-});
-
-router.post('/signup', upload.single('profilePicture'), async (req, res) => {
+router.post("/signup", upload.single("profilePicture"), async (req, res) => {
   const payload = req.body;
   console.log("Payload received:", payload);
 
@@ -77,7 +66,7 @@ router.post('/signup', upload.single('profilePicture'), async (req, res) => {
     };
 
     await User.create(newUser);
-    res.status(201).json({ message: 'User created' });
+    res.status(201).json({ message: "User created" });
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
@@ -85,28 +74,28 @@ router.post('/signup', upload.single('profilePicture'), async (req, res) => {
 });
 
 /* POST route to login */
-router.post('/login', async (req, res) => {
-  const payload = req.body; 
+router.post("/login", async (req, res) => {
+  const payload = req.body;
 
   try {
     const user = await User.findOne({ email: payload.email });
     if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+      return res.status(404).json({ error: "User not found" });
     }
 
     const isPasswordValid = bcrypt.compareSync(payload.password, user.password);
     if (!isPasswordValid) {
-      return res.status(401).json({ error: 'Invalid credentials' });
+      return res.status(401).json({ error: "Invalid credentials" });
     }
 
     const token = jwt.sign({ userId: user._id }, process.env.TOKEN_SECRET, {
-      expiresIn: '1d', // Token expires in 1 day
+      expiresIn: "1d", // Token expires in 1 day
     });
 
-    res.json({ message: 'Login successful', token });
+    res.json({ message: "Login successful", token });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: 'Error during login' });
+    res.status(500).json({ error: "Error during login" });
   }
 });
 
