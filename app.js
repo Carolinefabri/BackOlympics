e58754b6f4aedbc18e79a33f2ef3e13b5535e6bd
Brian Adams <brianadamsmaino68@gmail.com>
2023-08-06 09:29:10 +0200

    Delete extra space

--- app.js
+++ app.js
@@ -22,8 +22,10 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Something went wrong' });
 });
 
+const userRoutes = require("./routes/user.routes");
+app.use("/user", userRoutes);
 
-
+// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
 module.exports = app;
