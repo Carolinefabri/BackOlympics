779bfbd1e0211b3cf52a04e2a47cab88ab9e7194
Priscilla Riesegit config --global user.email priscilla.riese@gmail.com <priscilla.riese@gmail.com>
2023-08-07 19:38:51 +0200

    coxinha nada

--- app.js
+++ app.js
@@ -17,10 +17,11 @@ app.use((req, res) => {
 });
 
 // Middleware de tratamento de erro
-app.use((err, req, res, next) => {
+app.use(err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Something went wrong' });
-});
+  } );
+
 
 
 
